a8ebbc96a93152a1c180750130cd461c11ab2c42
manuelaceron <mafe-cv@hotmail.com>
2020-10-27 20:39:16 -0500

    Add some comments

--- Simple Perceptron/QuizAND.m
+++ Simple Perceptron/QuizAND.m
@@ -1,47 +1,61 @@
-%Perceptrón simple: AND
-%Juan Camilo Amaya
+%Simple Perceptron: Logic AND Function
 %Manuela Cerón Viveros
-%Juan Daniel Muñoz
 
 clc;
 clear all;
 close all;
-%Bias - Entrada 1 - Entrada 2
+
+%Bias - Input 1 - Input 2
 x=[1 1 1 1;0 0 1 1;0 1 0 1];
+
 %Target
 t=[-1 -1 -1 1];
-%Inicialización de los pesos
+
+N = size(t);
+N = N(1);
+
+%Weight initialization
 w0=1.5;
 w1=0.4;
 w2=0.5;
 W=[w0;w1;w2];
 
+%Variable to iterate over x and t
 k=1;
 o=0;
-d=0;
+delta=0;
 e=0;
-while o<4
+while o<N
     
-    if k>4
+    if k>N
         k=1;
     end
     
+    %Signum function
     y=sign(W(1)*x(1,k)+W(2)*x(2,k)+W(3)*x(3,k));
-    y=y+(1*(y==0))
-    d=t(k)-y
-    o=(o+1)*(d==0)
-    W=W+(1)*d*(x(:,k))
-    k=k+1
+    % y is 1 or -1
+    y=y+(1*(y==0));
+    % Delta calculation
+    delta=t(k)-y;
     
-    e=e+1 %Contador de iteraciones
+    %Move to the next value
+    o=(o+1)*(delta==0);
+    
+    %Weight calculation
+    W=W+(1)*delta*(x(:,k));
+    k=k+1;
+    
+    %Iterations counter
+    e=e+1; 
 end
 
 
 %%
-%Validación
-%Entrada
-x1=1;
+%Validation
+%Ingress the input
+x1=0;
 x2=1;
 
-%Cálculo de la capa oculta
+%With the given weights, the model calculates the result for AND function
+%The result will be 1 only when x1 and x2 are 1
 yv=sign(W(1)+W(2)*x1+W(3)*x2)
